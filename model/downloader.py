d11565922eb938c0d78210e5f19a08374566b768
SandeepMuhal88 <sandeepmuhal9982@gmail.com>
2026-03-31 15:03:15 +0530

    Update the code

--- model/downloader.py
+++ model/downloader.py
@@ -1,10 +1,9 @@
 from huggingface_hub import hf_hub_download
 
-file_path= hf_hub_download(
-    repo_id="TheBloke/Phi-3-mini-4k-instruct-GGUF",
-    filename="phi-3-mini-4k-instruct.Q4_K_M.gguf",
-    local_dir="models",
-    local_dir_use_symlinks=False
+file_path = hf_hub_download(
+    repo_id="lmstudio-community/Phi-3-mini-4k-instruct-GGUF",
+    filename="Phi-3-mini-4k-instruct-Q4_K_M.gguf",
+    local_dir="models"
 )
 
-print(f"Model downloaded to: {file_path}")
+print("Downloaded:", file_path)
